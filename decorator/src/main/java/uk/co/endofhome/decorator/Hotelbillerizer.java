161c180fabbfbebdbb0d281cddac343b870ec981
Tom Barnes <ruralmyth@gmail.com>
2016-10-23 14:08:30 +0100

    add Hotelbillerizer

---
+++ decorator/src/main/java/uk/co/endofhome/decorator/Hotelbillerizer.java
@@ -0,0 +1,28 @@
+package uk.co.endofhome.decorator;
+
+public class Hotelbillerizer {
+    public static void main(String[] args) {
+        System.out.println("BASIC STAY:");
+        HotelRoom basicStay = new SingleRoom();
+        System.out.println(basicStay.description());
+        System.out.println("€" + basicStay.cost());
+        System.out.println("");
+
+        System.out.println("THIRSTY CUSTOMERS:");
+        HotelRoom thirstyStay = new DoubleRoom();
+        thirstyStay = new MiniBarCola(thirstyStay);
+        thirstyStay = new MiniBarWater(thirstyStay);
+        System.out.println(thirstyStay.description());
+        System.out.println("€" + thirstyStay.cost());
+        System.out.println("");
+
+        System.out.println("ROCK STAR:");
+        HotelRoom rockStarStay = new Suite();
+        rockStarStay = new MiniBarBeer(rockStarStay);
+        rockStarStay = new LateCheckOut(rockStarStay);
+        rockStarStay = new BreakfastInBed(rockStarStay);
+        rockStarStay = new MiniBarBeer(rockStarStay);
+        System.out.println(rockStarStay.description());
+        System.out.println("€" + rockStarStay.cost());
+    }
+}
