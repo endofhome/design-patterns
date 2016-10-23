f5a7ecb9d56aab4d7c2fab5a95012100a08e99e5
Tom Barnes <ruralmyth@gmail.com>
2016-10-23 13:40:19 +0100

    add decorator abstract class

---
+++ decorator/src/main/java/uk/co/endofhome/decorator/RoomService.java
@@ -0,0 +1,5 @@
+package uk.co.endofhome.decorator;
+
+public abstract class RoomService extends HotelRoom {
+    public abstract String description();
+}
--- decorator/src/main/java/uk/co/endofhome/decorator/Suite.java
+++ decorator/src/main/java/uk/co/endofhome/decorator/Suite.java
@@ -4,7 +4,7 @@ public class Suite extends HotelRoom {
     public Suite() {
         description = "Luxury suite";
     }
-    
+
     @Override
     public double cost() {
         return 150.00;
